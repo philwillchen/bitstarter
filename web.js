37017baa1a0945b3ec3e1539abb90f1148265578
Phillip Chen <phillipchen@gmail.com>
2013-07-09 07:22:19 +0000

    test

---
+++ web.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  fs = require('fs')
+  fs.readFile('/index.html', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  });
+)
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
